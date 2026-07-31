3a4a74fff087b20c95bd837ca40d8e67deb81b57
Aquinozz <muriloaquinotrab@gmail.com>
2026-07-31 18:15:36 -0300

    chore: inicializa projeto Herald com Spring Boot 3.5.14

---
+++ src/test/java/Herald/Herald/HeraldApplicationTests.java
@@ -0,0 +1,13 @@
+package Herald.Herald;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeraldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
